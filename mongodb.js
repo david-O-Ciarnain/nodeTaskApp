b8d70a8792b72e192841c4b41f338be9ecdea67b
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2023-01-12 10:08:47 +0100

    setup user and tasks schemas in database

--- mongodb.js
+++ mongodb.js
@@ -1,30 +1,29 @@
-import mongodb, { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+
 
 const envConfig = dotenv.config();
 const env = process.env;
 
-const connectionURL = "mongodb://127.0.0.1:27017";
+const connectionURL = env.MONGODB_URL;
 const databaseName = env.DB_NAME;
 
-const client = new MongoClient(connectionURL);
+const client = new MongoClient(connectionURL, { useNewUrlParser: true });
 
 async function main() {
   await client.connect();
-  console.log("Connected successfully to db server");
+  console.log("Connected to database");
+
   const db = client.db(databaseName);
-  const collection = await db.collection("tasks");
-  await collection
-    .insertMany([
-      { description: "throw trash", completed: false },
-      { description: "read docs", completed: true },
-      { description: "play base", completed: false },
-    ])
-    .then(console.log);
+
+  const userCollection = db.collection("tasks");
+
+  await userCollection.deleteOne({ description: "throw trash" }).then(console.log);
+
   return "done";
 }
 
 main()
   .then(console.log)
   .catch(console.error)
-  .finally(() => client.close);
+ // .finally(() => client.close);
---
+++ src/db/mongoose.js
@@ -0,0 +1,79 @@
+import mongoose, { Mongoose } from "mongoose";
+import validator from "validator";
+import dotenv from "dotenv";
+
+const envConfig = dotenv.config();
+const env = process.env;
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manger-api", {
+  useNewUrlParser: true,
+});
+
+mongoose.set("strictQuery", true);
+
+const Users = mongoose.model("users", {
+  name: {
+    type: String,
+    validate(value) {
+      if (!validator.isAlpha(value))
+        throw new Error("Name can only be letters");
+    },
+    default: "user",
+    required: true,
+  },
+  email: {
+    type: String,
+    validate(value) {
+      if (!validator.isEmail(value)) throw new Error("Invalid email");
+    },
+    required:true
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) throw new Error("Age must be a postive number");
+    },
+    min: 18,
+  },
+});
+
+const Tasks = mongoose.model("tasks", {
+  description: {
+    type: String,
+    validate(value) {
+      if (!validator.isAlphanumeric(value))
+        throw new Error(
+          "Description can only be Alphanumeric no special characters"
+        );
+    },
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+  },
+});
+
+const task = new Tasks({
+  description: "Completed this section today[//#¤",
+  completed: false,
+});
+
+task
+  .save()
+  .then(() => console.log(task))
+  .catch((error) => console.log(error));
+
+/*const user = new User({
+  name: "Sven",
+  age: "error",
+});
+
+user
+  .save()
+  .then(() => {
+    console.log(user);
+  })
+  .catch((error) => {
+    console.log("Error" + error);
+  });
+*/
