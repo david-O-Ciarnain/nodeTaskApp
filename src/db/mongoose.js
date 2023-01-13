710e81841fd0731114ad56b90399c95417a57dc1
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2023-01-13 13:10:19 +0100

    change update for handle middelware

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,6 +8,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manger-api", {
 
 mongoose.set("strictQuery", true);
 
+userSchem.pre("save", async function (next) {
+  const user = this;
+});
+
 export const Users = mongoose.model("users", userSchem);
 
 export const Tasks = mongoose.model("tasks", taskSchem);
