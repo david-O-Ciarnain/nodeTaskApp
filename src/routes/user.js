96d15eaed0ca0b6fcb2ae7be1e5f7bbe03f35fd3
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2023-01-27 13:26:56 +0100

    only image upload and max sice 2mb

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,6 +10,15 @@ import {
 
 const uploadFile = multer({
   dest: "avatar",
+  limits: {
+    fileSize: 2000008,
+  },
+  fileFilter(req, file, callback) {
+    if (file.originalname.match(/\.(.png|.jpg|.jpeg)$/)) {
+      return callback(new Error("Please upload a image"));
+    }
+    callback(undefined, true);
+  },
 });
 
 router.post("/users", async (req, res) => {
